a2b3d1dc1ca3a6635f8cba2dd442e334c5b01f3e
K Sharma <sharma.1809k@gmail.com>
2019-03-05 22:44:21 -0500

    updates

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://ds053708.mlab.com:53708/heroku_blkn1ggq"
+  process.env.MONGODB_URI || "mongodb://localhost/usersscorelist",
+ // "mongodb://clickygame:clickygame1@ds053708.mlab.com:53708/heroku_blkn1ggq"
+ {
+   useNewUrlParser: true
+ }
 );
 
 // Start the API server
